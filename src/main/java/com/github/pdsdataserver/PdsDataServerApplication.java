5d2d35fbd6995402a46b6b7fe1ca8451b0105a20
AldricVS <aldric.silvestre@outlook.fr>
2022-03-23 21:17:17 +0100

    Create project with (normally) all dependencies

---
+++ src/main/java/com/github/pdsdataserver/PdsDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.pdsdataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsDataServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pdsdataserver/PdsDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pdsdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsDataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
